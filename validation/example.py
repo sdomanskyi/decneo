3c65d449659d4faf6a9e8779fe005a409e748a27
michmeng <49258208+michmeng@users.noreply.github.com>
2020-12-08 13:31:30 -0800

    update example.py

--- validation/example.py
+++ validation/example.py
@@ -19,25 +19,31 @@ endo_goi = ['KDR','FLT1', 'FLT4', 'NRP1', 'NRP2', 'FGFR1', 'FGFR2','FGFR3', 'CXC
 new_rec = "/mnt/home/paterno1/PL/receptorsListHugo_2555.txt"
 all_recs =list(set( [x.strip() for x in open(new_rec).readlines()] + endo_goi))
 
-## Run analysis on first species only 
+# Get endothelial cell ids
+endo_cell_df = pd.read_csv("/mnt/home/paterno1/Michelle/Validation/endo_cells.csv", index_col = 0)
+endo_cells_list = list(endo_cell_df.columns)
+
+## Run analysis on first species only, set hum_endo_list to list of endothelial cell ids in human_count_df
 out_dir = "results_hum/"
 human_count_df = pd.read_csv("/mnt/home/paterno1/Michelle/Validation/human_count.csv", index_col = 0)
+hum_endo_list = [value for value in endo_cells_list if value in list(human_count_df.columns)] 
 
 human_gene_stats_mk = dd.full_data_process(human_count_df,
                        out_dir,
-                       None,
+                       hum_endo_list,
                        genes2=all_recs,
                        diff_expr = None,
                        markers = endo_goi)
 
-## Run analysis on second species, allowing it to also calculate conservation 
+## Run analysis on second species and set mus_endo_list to list of endothelial cell ids in mouse_count_df, allowing it to also calculate conservation 
 out_dir = "results_mus/"
 mouse_count_df = pd.read_csv("/mnt/home/paterno1/Michelle/Validation/mouse_count.csv", index_col = 0)
-human_dendro = pd.read_csv("/mnt/home/paterno1/Michelle/Validation/results/gene_stats_Full.csv", index_col = 0)
+mus_endo_list = [value for value in endo_cells_list if value in list(mouse_count_df.columns)] 
+human_dendro = pd.read_csv("/mnt/home/paterno1/Michelle/Validation/results_hum/gene_stats_Full.csv", index_col = 0)
 
 mouse_gene_stats_mk = dd.full_data_process(mouse_count_df,
                        out_dir,
-                       None,
+                       mus_endo_list,
                        genes2=all_recs,
                        diff_expr = None,
                        compare_dendro= human_dendro,
